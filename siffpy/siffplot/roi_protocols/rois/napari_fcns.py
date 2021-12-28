eb69801137a4dd881d0772802fd97cc4aaf7bb27
Stephen <thornquist@fas.harvard.edu>
2021-12-27 20:11:29 -0500

    Expanded napari toolkit, event tracking in metadata

--- siffpy/siffplot/roi_protocols/rois/napari_fcns.py
+++ siffpy/siffplot/roi_protocols/rois/napari_fcns.py
@@ -10,14 +10,72 @@ image interactions.
 
 SCT 12/11/2021
 """
+from typing import Callable
 import numpy as np
 import napari
 from napari.layers.shapes import Shapes
 import holoviews as hv
 
 __all__ = [
+    'get_largest_lines_napari',
     'get_largest_polygon_napari'
 ]
+
+#######################
+### PRIVATE METHODS ###
+#######################
+
+def _slice_idx_parsing(slice_idx, shapes_to_array_fcn : Callable, ret_tuple_fcn : Callable, n_returned : int, shapes_layer : Shapes):
+    """
+    Generic operation used by several functions here. Parses the argument
+    slice_idx, which could be of type int, None, or list[int]. Based on which it is,
+    returns either a tuple, a list of tuples, or a list of list of tuples of holoviews
+    elements, determined by ret_tuple_fcn.
+
+    Returns None (rather than a tuple) for cases where no eligible shapes are extracted.
+    """
+
+    list_of_arrays = shapes_to_array_fcn(shapes_layer) # list of numpy arrays of points
+
+    if slice_idx is None:
+        # Get the desired object irrespective of slice
+        if len(list_of_arrays) == 0:
+            return None
+        return ret_tuple_fcn(list_of_arrays, n_returned) # all polygons are viable!
+    
+    if type(slice_idx) is int:
+        # Take only the ones in the correct slice plane
+        slice_arrays = [
+            individual_array
+            for individual_array in list_of_arrays
+            if (individual_array.shape[-1] == 3) and np.all(individual_array[:,0] == slice_idx)
+        ]
+        if len(slice_arrays) == 0:
+            return None
+        return ret_tuple_fcn(slice_arrays, n_returned)
+
+    if type(slice_idx) is list:
+        ret_list = []
+        for this_slice in slice_idx:
+            try:
+                this_slice = int(this_slice)
+            except:    
+                raise TypeError("At least one element of argument slice_idx cannot be cast to type int")
+
+            slice_arrays = [
+                individual_array
+                for individual_array in list_of_arrays
+                if (individual_array.shape[-1] == 3) and np.all(individual_array[:,0] == this_slice)
+            ]
+
+            if len(slice_arrays) == 0:
+                ret_list.append(None)
+            else:
+                ret_list.append(ret_tuple_fcn(slice_arrays, n_returned))
+
+        return ret_list
+    
+    raise TypeError(f"Argument for slice_idx is {slice_idx}, not of type int, list, or None")
 
 def _polygon_area(x_coords : np.ndarray, y_coords : np.ndarray) -> float:
     """ Shoelace method to compute area"""
@@ -41,6 +99,7 @@ def _shapes_to_polys(shape_layer : Shapes) -> list[np.ndarray]:
 
     polys : list of numpy.ndarrays
     """
+
     return [shape_layer.data[idx] for idx in range(len(shape_layer.data)) if shape_layer.shape_type[idx] == 'polygon']
 
 def _shapes_to_lines(shape_layer : Shapes) -> list[np.ndarray]:
@@ -57,8 +116,9 @@ def _shapes_to_lines(shape_layer : Shapes) -> list[np.ndarray]:
     Returns
     -------
 
-    liness : list of numpy.ndarrays
+    lines : list of numpy.ndarrays
     """ 
+
     return [shape_layer.data[idx] for idx in range(len(shape_layer.data)) if shape_layer.shape_type[idx] == 'line']
 
 def _largest_polygon_tuple_from_viable(polys_list, n_polygons : int = 1) -> tuple[hv.element.path.Polygons,int, int]:
@@ -103,6 +163,71 @@ def _largest_polygon_tuple_from_viable(polys_list, n_polygons : int = 1) -> tupl
         )
     return ret_list
 
+def _largest_lines_tuple_from_viable(
+    line_list : list[np.ndarray], n_lines : int = 1
+    )->list[hv.element.path.Path, int, int]:
+    """
+    Extracts the longest lines and returns them as tuples
+    as desired by get_largest_lines
+    """
+    lengths = [(line[0,-1]-line[1,-1])**2 + (line[0,-2] + line[1,-2])**2 for line in line_list] # ignoring sqrt
+    roi_idxs = np.argpartition(np.array(lengths), -n_lines)[-n_lines:]
+    ret_list = []
+    # If there's only one, don't bother returning as a list
+    if n_lines == 1:
+        line_array = line_list[roi_idxs[-1]]
+        if line_array.shape[-1] == 2:
+            ret_slice = None
+        else:
+            ret_slice = int(line_array[0][0])
+        return (
+            hv.Path(
+                {('y','x'):line_array[:,-2:]}
+            ),
+            ret_slice,
+            roi_idxs[-1]
+        )
+
+    # If there's more than one line requested,
+    # return a list of lines
+    for idx in roi_idxs:
+        line_array = line_list[idx]
+        if line_array.shape[-1] == 2:
+            ret_slice = None
+        else:
+            ret_slice = int(line_array[0][0])
+        ret_list.append(
+            (
+                hv.Path(
+                    {('y','x'):line_array[:,-2:]}
+                ),
+                ret_slice,
+                idx
+            )
+        )
+    return ret_list
+
+##########################
+### IMPORTABLE METHODS ###
+##########################
+def get_largest_lines_napari(
+        viewer : napari.Viewer,
+        shape_layer_name : str = 'ROI shapes',
+        slice_idx : int = None,
+        n_lines = 2
+    ) -> tuple[hv.element.path.Path, int, int]:
+    """
+    Extracts the longest line type shapes from the given
+    napari Viewer and layer. 
+    """
+     # get the layer matching the name expected. Throws an
+    # error if no such layer.
+    lines_layer = next(filter(lambda x: x.name == shape_layer_name, viewer.layers),None) 
+    if not type(lines_layer) is napari.layers.shapes.shapes.Shapes:
+        raise TypeError(f"Specified layer {shape_layer_name} is not a layer with lines (i.e. ShapesLayer)")
+
+    return _slice_idx_parsing(slice_idx, _shapes_to_lines, _largest_lines_tuple_from_viable, n_lines, lines_layer)
+
 def get_largest_polygon_napari(
         viewer : napari.Viewer, 
         shape_layer_name : str = 'ROI shapes',
@@ -127,35 +252,4 @@ def get_largest_polygon_napari(
     if not type(poly_layer) is napari.layers.shapes.shapes.Shapes:
         raise TypeError(f"Specified layer {shape_layer_name} is not a layer of polygons (shapes)")
 
-    polys = _shapes_to_polys(poly_layer) # list of numpy arrays of points
-
-    if slice_idx is None:
-        # Get the largest polygon irrespective of slice
-        return _largest_polygon_tuple_from_viable(polys, n_polygons) # all polygons are viable!
-    
-    if type(slice_idx) is int:
-        # Take only the ones in the correct slice plane
-        slice_polys = [
-            poly
-            for poly in polys
-            if (poly.shape[-1] == 3) and np.all(poly[:,0] == slice_idx)
-        ]
-
-        return _largest_polygon_tuple_from_viable(slice_polys, n_polygons)
-
-    if type(slice_idx) is list:
-        ret_list = []
-        for this_slice in slice_idx:
-            if not type(this_slice) is list:
-                raise TypeError("At least one element of argument slice_idx is not of type int")
-            slice_polys = [
-                poly
-                for poly in polys
-                if (poly.shape[-1] == 3) and np.all(poly[:,0] == this_slice)
-            ]
-
-            ret_list.append(_largest_polygon_tuple_from_viable(slice_polys, n_polygons))
-
-        return ret_list
-    
-    raise TypeError(f"Argument for slice_idx is {slice_idx}, not of type int, list, or None")
+    return _slice_idx_parsing(slice_idx, _shapes_to_polys, _largest_polygon_tuple_from_viable, n_polygons, poly_layer)
