Added SiffEvents, fixed the final reference frame computation in registration

siffpy/sifftrac/plotters/trajectoryplotter.py
@@ -34,8 +34,8 @@ class TrajectoryPlotter(TracPlotter):
                 np.min(log.dataset['integrated_position_lab_1']),
                 np.max(log.dataset['integrated_position_lab_1'])
             )
-            square_bounds = (np.min(bounds), np.max(bounds))
-            this_path = this_path.opts(xlim=square_bounds,ylim=square_bounds)
+            #square_bounds = (np.min(bounds), np.max(bounds))
+            this_path = this_path.opts(aspect='equal')
         if not kwargs is None:
             this_path.opts(**kwargs)
 

siffpy/siffutils/registration.py
@@ -468,7 +468,7 @@ def register_frames(frames : list[int], **kwargs)->tuple[dict, np.ndarray, np.nd
     ysize,xsize = frames_np[0].shape
 
     # rebuild the reference images for storage.
-    ref_im = build_reference_image(frames, **kwargs)
+    ref_im = build_reference_image(frames, registration_dict = reg_dict, **kwargs)
 
     roll_d_array = np.array([roll_d(rolls[n], rolls[n+1], ysize, xsize) for n in range(len(frames)-1)])
 