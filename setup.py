e1aa91fceb2ed72d871db8624d0f71f003e5f906
Stephen <thornquist.stephen@gmail.com>
2023-01-16 20:26:50 -0500

    moved siffreadermodule out of the main directory

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, Extension
+
+try:
+    import numpy
+except ImportError as error:
+    raise Exception("Numpy is not yet installed on this distribution. Set up numpy using command 'pip install <directory_containing_this_setup.py>' instead.")
+
+# written for Darwin. Probably should write a Windows and/or GNU compatible
+siffmodule = Extension(
+    name='siffpy.siffreadermodule',
+    sources = [
+        'siffreadermodule/src/siffio.cpp',
+        'siffreadermodule/src/siffreader.cpp',
+        'siffreadermodule/src/siffreadermodule.cpp',
+        'siffreadermodule/src/sifftotiff.cpp',
+    ],
+    include_dirs = [
+        numpy.get_include()
+    ],
+    extra_compile_args=["-stdlib=libc++"],
+    language="c++",
+)
+
+setup (
+    name='siffpy.siffreadermodule',
+    ext_modules = [siffmodule],
+)
