75695b37402caaa2b08f3139d13e24c8cedca7a7
Stephen <thornquist.stephen@gmail.com>
2023-02-21 23:18:45 -0500

    shame shame

--- siffpy/core/__init__.py
+++ siffpy/core/__init__.py
@@ -1,3 +1,6 @@
-from siffpy.core._version import __version__
+try:
+    from siffpy.core._version import __version__
+except ImportError:
+    from siffpy.core.utils.shame import __version__
 from siffpy.core.siffreader import SiffReader
 from siffpy.core.flim import FLIMParams, fit_exp
